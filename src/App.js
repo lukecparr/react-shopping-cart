206e21512616ef3d9310345484d93060b8f926eb
Lukecp <lukecp@pm.me>
2021-02-04 22:33:31 -0500

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// Dependencies
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
-import data from './data';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+// Other
+import { productContext } from './contexts/productContext';
+import { cartContext } from './contexts/cartContext';
+import data from './data';
 
 function App() {
 	const [products] = useState(data);
@@ -13,22 +18,27 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    <div className="App">
+      <productContext.Provider value={{ products, addItem }}>
+        <cartContext.Provider value={cart}>
+          <Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </cartContext.Provider>
+      </productContext.Provider>
+    </div>
+  );
 }
 
 export default App;
